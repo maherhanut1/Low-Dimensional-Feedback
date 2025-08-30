2dbea73515c532c709fe86c98dfc7a7eb6d75709
maherhanut1 <maher.hanut@gmail.com>
2025-08-31 02:25:17 +0300

    fix ldfa value replacment

--- training_utils/trainer.py
+++ training_utils/trainer.py
@@ -9,7 +9,7 @@ from typing import List, Callable, Tuple
 def replace_linear(module):
 	for name, child in module.named_children():
 		if isinstance(child, LDFA_Linear):
-			child.is_LDFA.data = 1.0
+			child.is_LDFA.data.fill_(1.0)
 			print(f"Replaced {name} with LDFA_Linear")
 		else:
 			replace_linear(child)
